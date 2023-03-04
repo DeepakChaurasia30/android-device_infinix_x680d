688277e9f90df8472228cc69481e9c731523a075
DeepakChaurasia30 <chaurasia.deepak.300@gmail.com>
2023-03-04 15:19:11 +0530

    intilized:x680d

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2021 Teracube Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_x680d.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_x680d-userdebug
---
+++ lineage_x680d.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 Teracube Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from 2e device
+$(call inherit-product, device/infinix/x680d/device.mk)
+
+# Inherit from those products. Most specific first.
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_DEVICE := x680d
+PRODUCT_MANUFACTURER := Infinix
+PRODUCT_NAME := lineage_x680d
+PRODUCT_MODEL := Infinix x680d
+PRODUCT_GMS_CLIENTID_BASE := android-infinix
+TARGET_VENDOR := infinix
+TARGET_VENDOR_PRODUCT_NAME := x680d
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="full_x680d_h6116-user 10 QP1A.190711.020 21024 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Infinix/X680D-IN/Infinix-X680D:10/QP1A.190711.020/J-AS-200614V138:user/release-keys
